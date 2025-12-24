e42e07332876f66dc8f2d1943d7d62235074c098
kittinan4452 <kittinan4452@gmail.com>
2025-12-24 13:33:54 +0700

    ีีupdate_config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/tii-kittinan-pages",
+    // basePath: "/tii-kittinan-pages", // Uncomment for GitHub Pages deployment
     output: "export",
     images: {
         unoptimized: true,
